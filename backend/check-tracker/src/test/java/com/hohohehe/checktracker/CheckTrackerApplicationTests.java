569e20cdbd904a66d878df79dc66c769e77e6e2c
nsh <nsh2398@naver.com>
2025-05-19 23:52:16 +0900

    feat: backend 프로젝트 생성
    - Spring boot 3, gradle로 환경 세팅
    - dependency는 web, jpa, lombok, devtools, mysql 세팅

---
+++ backend/check-tracker/src/test/java/com/hohohehe/checktracker/CheckTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hohohehe.checktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

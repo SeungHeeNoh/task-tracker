fb481ae149f1e1ae34ceb95b7a4cc24d8123d148
nsh <nsh2398@naver.com>
2025-05-20 22:01:29 +0900

    feat: AuditingFields에 빠진 애노테이션 추가
    - CreatedDate, CreatedBy, LastModifiedDate, LastModifiedBy 애노테이션 추가
    
    #3

--- backend/check-tracker/src/main/java/com/hohohehe/checktracker/domain/AuditingFields.java
+++ backend/check-tracker/src/main/java/com/hohohehe/checktracker/domain/AuditingFields.java
@@ -5,6 +5,10 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -15,15 +19,19 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 public abstract class AuditingFields {
 
+    @CreatedDate
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
+    @CreatedBy
     @Column(nullable = false)
     private String createdBy;
 
+    @LastModifiedDate
     @Column
     private LocalDateTime modifiedAt;
 
+    @LastModifiedBy
     @Column
     private String modifiedBy;
 }

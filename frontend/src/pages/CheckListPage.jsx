5bddb0448b11c5f2051830a58e1f6dcd3409a888
nsh <nsh2398@naver.com>
2025-05-31 01:33:38 +0900

    feat: mode에 따라 view가 달라지도록 구현
    - mode가 list이면 체크리스트 목록 컴포넌트가 노출되도록 처리
    - mode가 add이면 체크리스트 등록 컴포넌트가 노출되도록 처리
    
    #6

--- frontend/src/pages/CheckListPage.jsx
+++ frontend/src/pages/CheckListPage.jsx
@@ -1,19 +1,26 @@
+import { useState } from 'react';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
 
 import '../assets/css/CheckListPage.css'
 
 import CheckListGroup from '../components/CheckListGroup';
+import CheckListForm from '../components/CheckListForm';
 
 export default function CheckListPage() {
+
+    const[mode, setMode] = useState('list');    // mode : list | add
 
     return (
         <>
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
                 <h1><FontAwesomeIcon icon={faSquareCheck} style={{color: "#74C0FC",}} /> 나의 체크리스트</h1>
-                <button className="add-button">+ 항목 추가</button>
+                { mode === 'list' && (<button onClick={() => setMode('add')} className="add-button">+ 항목 추가</button>) }
+                { mode === 'add' && (<button onClick={() => setMode('list')} className="add-button">← 돌아가기</button>)}
             </div>
-            <CheckListGroup />
+            { mode === 'list' && <CheckListGroup />}
+            { mode === 'add' && <CheckListForm />}
         </>
     );
 }

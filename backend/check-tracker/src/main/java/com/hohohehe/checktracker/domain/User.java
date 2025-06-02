b30e5263d12aafece31041586bf300b014b30ed9
nsh <nsh2398@naver.com>
2025-06-02 23:09:46 +0900

    feat: User entity 생성
    - 인증 객체로 사용하기 위해 UserDetails를 상속
    - equals, hashcode 메소드 선언
    - 인증 이후 자격 증명이 필요하지 않으므로 CredentailsContainer 인터페이스 구현
    
    #24

---
+++ backend/check-tracker/src/main/java/com/hohohehe/checktracker/domain/User.java
@@ -0,0 +1,58 @@
+package com.hohohehe.checktracker.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.CredentialsContainer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@ToString(callSuper = true)
+@Table(name = "users")
+@Entity
+public class User extends AuditingFields implements UserDetails, CredentialsContainer {
+
+    @Id
+    private String userId;
+
+    @Setter
+    @Column(nullable = false)
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(userId, user.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, password);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return userId;
+    }
+
+    @Override
+    public void eraseCredentials() {
+        this.password = null;
+    }
+}

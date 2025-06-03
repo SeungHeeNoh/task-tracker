cba5c3117c5774cef8f16cbbb214d8ea26f0dbdf
nsh <nsh2398@naver.com>
2025-06-03 17:27:11 +0900

    feat: UserRepository 생성
    - UserDetailsService의 loadUserByUsername 메소드에서 사용하기 위해 findByUserId 메소드 선언
    - 단위 테스트 코드 작성
    
    #24

---
+++ backend/check-tracker/src/main/java/com/hohohehe/checktracker/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.hohohehe.checktracker.repository;
+
+import com.hohohehe.checktracker.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUserId(String userId);
+}

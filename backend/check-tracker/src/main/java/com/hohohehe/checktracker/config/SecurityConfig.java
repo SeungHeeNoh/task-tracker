a359698b5c9c751f8f5a95d71211dff8caa9ed35
nsh <nsh2398@naver.com>
2025-06-06 18:56:15 +0900

    feat: jwt 세팅 및 관련 class 선언
    - jwt 관련 dependency 세팅
    - properteis에 jwt 관련 값을 세팅하고 이를 java단에서 사용할 수 있도록 JwtProperties 클래스 생성
    - 토근 생성/검증을 담당할 TokenProvider 생성
    - TokenProvider내에서 검증 중 관련 exception이 발생할 경우 사용할 Custom exception 생성(JwtAuthenticationException)
    - JWT 유효성 검사, SecurityContext 인증 정보 설정할 JWTAuthenticationFilter를 선언하고 SecurityConfig에 해당 필터를 목록에 추가
    
    #25

--- backend/check-tracker/src/main/java/com/hohohehe/checktracker/config/SecurityConfig.java
+++ backend/check-tracker/src/main/java/com/hohohehe/checktracker/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.hohohehe.checktracker.config;
 
+import com.hohohehe.checktracker.config.jwt.JWTAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,9 +9,13 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
+
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -20,6 +26,7 @@ public class SecurityConfig {
                                 .requestMatchers("/api/v1/auth/**").permitAll()
                                 .anyRequest().authenticated()
                 )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
---
+++ backend/check-tracker/src/main/java/com/hohohehe/checktracker/config/jwt/JWTAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package com.hohohehe.checktracker.config.jwt;
+
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    private final UserDetailsService userDetailsService;
+    private final TokenProvider tokenProvider;
+    private final static String HEADER_AUTHORIZATION = "Authorization";
+    private final static String TOKEN_PREFIX = "Bearer ";
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader(HEADER_AUTHORIZATION);
+        String token = extractToken(authorizationHeader);
+
+        if(token != null && tokenProvider.validToken(token)) {
+            try {
+                String userId = tokenProvider.getUserId(token);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(userId);
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch ( JwtException | IllegalArgumentException e ) {
+                log.warn("JWT token validation failed: {}", e.getMessage());
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractToken(String authorizationHeader) {
+        if (authorizationHeader != null && authorizationHeader.startsWith(TOKEN_PREFIX)) {
+            return authorizationHeader.substring(TOKEN_PREFIX.length());
+        }
+        return null;
+    }
+}
+

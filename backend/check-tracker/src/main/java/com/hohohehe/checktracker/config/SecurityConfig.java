a73e491c45b411ffa70f26cfc1cb0822816af168
nsh <nsh2398@naver.com>
2025-06-07 18:08:36 +0900

    feat: 로그인 시도시 db를 통해 등록된 user인지 확인하고 accessToken을 반환하도록 구현
    - ID, 비밀번호 기반으로 사용자를 인증하는 AuthenticationManager가 Spring boot3에서는 자동으로 빈으로 등록되지 않기 때문에 직접 등록 처리
    - 관련 LoginRequest/LogiinResponse 반환
    - 관련 테스틑 진행하기 위해 AuthControllerTest 생성.
    - SpringBootTest 진행 시 test 전용 properties를 사용하기 위해 application-test.yml 생성
    
    #25

--- backend/check-tracker/src/main/java/com/hohohehe/checktracker/config/SecurityConfig.java
+++ backend/check-tracker/src/main/java/com/hohohehe/checktracker/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import com.hohohehe.checktracker.config.jwt.JWTAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,5 +30,10 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }

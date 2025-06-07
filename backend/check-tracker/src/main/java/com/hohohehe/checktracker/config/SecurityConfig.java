dbe6c47df260f11e3af33a8235f7f64377338b7e
nsh <nsh2398@naver.com>
2025-06-07 17:38:36 +0900

    feat: jwt 세팅 후 발생하는 순환 참조 이슈 해결
    JWTAuthenticationFilter -> UserServiceImpl ->  SecurityConfig 구조에서 순환참조 발생하여 프로젝트가 구동되지 못하는 이슈 발생
    확인 결과 SecurityConfig를 초기화하기 위해 JWTAuthenticationFilter를 만들다가 UserServiceImpl 를 생성하려고 하니 다시 SecurityConfig의 PasswordEncoder가 필요한 상황 때문에 SecurityConfig를 건드리면서 순환 참조 발생
    따라서 이를 해결하기 위해 SecurityConfig 내에 선언된 PasswordEncoder bean을 제거하고 PasswordConfig configuration에서 관리하도록 처리
    
    #25

--- backend/check-tracker/src/main/java/com/hohohehe/checktracker/config/SecurityConfig.java
+++ backend/check-tracker/src/main/java/com/hohohehe/checktracker/config/SecurityConfig.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -29,10 +27,5 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }

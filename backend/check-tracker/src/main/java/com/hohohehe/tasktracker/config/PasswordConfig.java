542b79c4b4d9ca6884eaf84644ad38bbd5045f29
nsh <nsh2398@naver.com>
2026-02-18 19:49:20 +0900

    feat: spring security 기본 설정 복구
    #23

--- backend/check-tracker/src/main/java/com/hohohehe/tasktracker/config/PasswordConfig.java
+++ backend/check-tracker/src/main/java/com/hohohehe/tasktracker/config/PasswordConfig.java
@@ -1,15 +1,15 @@
-//package com.hohohehe.checktracker.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//@Configuration
-//public class PasswordConfig {
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//    }
-//}
+package com.hohohehe.tasktracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
